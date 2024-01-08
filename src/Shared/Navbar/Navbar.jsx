614d8f823e8a15ee60e8b34e6307231b06783513
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2024-01-08 17:30:51 +0600

    moduel 76.5 practice

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,12 +1,11 @@
-
 const Navbar = () => {
-    return (
-      <div className="max-w-screen-xl mx-auto">
-        <div className="bg-purple-400">
-          <p className="text-center">This is navbar</p>
-        </div>
+  return (
+    <div className="max-w-screen-xl mx-auto">
+      <div className="bg-purple-400">
+        <p className="text-center">This is navbar</p>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Navbar;
